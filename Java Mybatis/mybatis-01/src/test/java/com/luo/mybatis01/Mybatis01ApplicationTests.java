47c7a6d47c5de7d5fa30976d4e51863fc28b08bf
洛臣心 <55872657+luoxi-1314@users.noreply.github.com>
2020-02-27 22:48:00 +0800

    w
    w

---
+++ Java Mybatis/mybatis-01/src/test/java/com/luo/mybatis01/Mybatis01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luo.mybatis01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
